7b991236f0646e9fb5dedb700fb8f686c2eecc1b
Sudharsan4449 <sudharsan4449@gmail.com>
2026-04-03 05:12:40 +0530

    fix health route

--- server/routes/health.js
+++ server/routes/health.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(404).json({ status: 'OK' });
+  res.status(200).send('OK');
 });
 
 module.exports = router;
